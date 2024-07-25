48fb8d59e7dffd08dd7938c9ed92e61dd08a4fae
Damien <amanvilledamien@hotmail.fr>
2024-07-25 15:38:05 +0400

    fix shadow 3Dmodel

--- app/utils/Scene.jsx
+++ app/utils/Scene.jsx
@@ -2,17 +2,19 @@
 'use client';
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls,Environment } from '@react-three/drei';
-
 import Model from './Model';
+import { OrbitControls,Environment} from '@react-three/drei';
+
 
 export default function Scene() {
+
+  
   return (
-    <div className='h-screen absolute w-screen flex '>
-      <Canvas>
-        <directionalLight position={[0, 3, 2]} intensity={5} />
-        <Environment preset='city'/>
+    <div className='h-screen w-full p-20 flex '>
+      <Canvas >
         <Model />
+        <ambientLight position={[0, 3, 2]} intensity={5} />
+        <Environment preset="city"/>
         <OrbitControls />
       </Canvas>
     </div>
