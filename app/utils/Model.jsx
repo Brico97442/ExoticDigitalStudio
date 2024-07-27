a2661472377ca7418296e64891c0745fa4b742a5
Damien <amanvilledamien@hotmail.fr>
2024-07-27 16:00:48 +0400

    create opacity background effect on of canvas

--- app/utils/Model.jsx
+++ app/utils/Model.jsx
@@ -1,43 +1,48 @@
-
-import React, { useRef } from 'react'
-import { useGLTF, Text, MeshTransmissionMaterial } from "@react-three/drei";
-import { useFrame, useThree } from '@react-three/fiber'
-import { useControls } from 'leva';
-
+import React, { useRef, useEffect } from 'react';
+import { useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from '@react-three/fiber';
+import gsap from 'gsap';
 
 export default function Model() {
     const { nodes } = useGLTF("/media/reunion.glb");
-    const { viewport } = useThree()
-    const island = useRef(null);
+    const { viewport } = useThree();
+    const island = useRef();
+    const materialRef = useRef();
 
-    const materialProps = useControls({
-
-        thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
-
-        roughness: { value: 0, min: 0, max: 1, step: 0.1 },
-
-        transmission: {value: 1, min: 0, max: 1, step: 0.1},
-
-        ior: { value: 1.2, min: 0, max: 3, step: 0.1 },
-
-        chromaticAberration: { value: 0.02, min: 0, max: 1},
-
-        backside: { value: true},
-
-    })
-
-    useFrame((state, delta) => {
+    useEffect(() => {
         if (island.current) {
-          island.current.rotation.y += 0.002; // Rotation sur l'axe Y
+            // Animation de la position de l'île
+            gsap.from(island.current.position, {
+                y: -1,
+                x: 1,
+                duration: 2
+            });
+            gsap.to(island.current.position, {
+                z: -4,
+                duration: 2
+            });
+            // Configuration initiale du matériau
+            materialRef.current.color.set("#0f4c5c");
+            materialRef.current.transparent = true;
+            materialRef.current.opacity = 0.5; // Mettre l'opacité à 50%
         }
-      });
-    
+    }, []);
+
+    useFrame(() => {
+        if (island.current) {
+            // Rotation sur l'axe Y
+            // island.current.rotation.y += 0.001;
+
+            // Exemple d'animation de l'opacité (oscillation)
+            // materialRef.current.opacity = 0.5 + 0.5 * Math.sin(Date.now() * 0.001);
+        }
+    });
 
     return (
-        <group scale={viewport.width /3.5}position={[0,0,-6]}>
-            <mesh  ref={island} {...nodes.reunion}>
-            <MeshTransmissionMaterial {...materialProps}/>
+        <group scale={viewport.width / 3.5}>
+            <mesh ref={island} geometry={nodes.reunion.geometry}>
+                <meshStandardMaterial ref={materialRef} />
             </mesh>
         </group>
-    )
+    );
 }
--- app/utils/Scene.jsx
+++ app/utils/Scene.jsx
@@ -1,21 +1,33 @@
-// Scene.js
 'use client';
-import React from 'react';
+import React, { useEffect, useRef,Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import Model from './Model';
-import { OrbitControls,Environment} from '@react-three/drei';
-
+import { OrbitControls, Environment } from '@react-three/drei';
+import { gsap } from 'gsap';
 
 export default function Scene() {
+  const divRef = useRef(null);
 
-  
+  useEffect(() => {
+    // Animation de la couleur de fond du noir à transparent
+    gsap.to(divRef.current, {
+      backgroundColor: 'rgba(0, 0, 0, 0)',
+      duration: 2,
+      delay: 5,
+      zIndex:0,
+    });
+    
+  }, []);
+
   return (
-    <div className='h-screen absolute w-full flex z-200]'>
-      <Canvas >
-        <Model/>
+    <div ref={divRef} className='h-screen fixed w-screen flex z-[206]' style={{ backgroundColor: 'black' }}>
+      <Canvas>
+        <Suspense>
+          <Model />
+        </Suspense>
         <ambientLight position={[0, 3, 2]} intensity={3} />
-        <Environment preset="forest"/>
-        {/* <OrbitControls /> */}
+        <Environment preset="forest" />
+        <OrbitControls />
       </Canvas>
     </div>
   );
