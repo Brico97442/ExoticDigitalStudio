8250d142a8e09f7f9c1d53305734fd5b0705b063
Damien <amanvilledamien@hotmail.fr>
2024-07-31 19:57:32 +0400

    fix animation bug on text

--- app/utils/Model.jsx
+++ app/utils/Model.jsx
@@ -50,15 +50,15 @@ export default function Model({ mousePosition }) {
         // Animation de la respiration pour les textes
         if (textRef1.current && textRef2.current) {
             gsap.to(textRef1.current.material, {
-                opacity: 0.5,
-                duration: 1,
+                opacity: 0.1,
+                duration: 1.5,
                 repeat: -1,
                 yoyo: true,
                 ease: 'power1.inOut'
             });
             gsap.to(textRef2.current.material, {
-                opacity: 0.5,
-                duration: 1,
+                opacity: 0.1,
+                duration: 1.5,
                 repeat: -1,
                 yoyo: true,
                 ease: 'power1.inOut'
