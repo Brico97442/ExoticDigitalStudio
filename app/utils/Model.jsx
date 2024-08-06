83ac56d76a38238c174fd3a2e5ef2fb8c1629bd1
Damien <amanvilledamien@hotmail.fr>
2024-08-06 18:16:31 +0400

    create on scroll effect on island mesh

--- app/utils/Model.jsx
+++ app/utils/Model.jsx
@@ -1,50 +1,25 @@
+gsap.registerPlugin(ScrollTrigger) 
 import React, { useRef, useEffect, useState } from 'react';
 import { useGLTF, Text } from '@react-three/drei';
 import { useFrame, useLoader, useThree } from '@react-three/fiber';
 import { LineSegments, BufferGeometry, Float32BufferAttribute, LineBasicMaterial, TextureLoader } from 'three';
 import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/all'
+import { inherits } from 'util';
 
-export default function Model({ mousePosition }) {
+export default function Model({ mousePosition, island }) {
     const { nodes } = useGLTF('/media/reunion2.glb');
     const { viewport } = useThree();
-    const island = useRef(null);
     const islandMaterialRef = useRef(null);
     const texture = useLoader(TextureLoader, '/media/texture.webp');
     const textRef1 = useRef(null);
     const textRef2 = useRef(null);
+
     const [lines, setLines] = useState(null);
 
     const location = useRef(null);
     const { nodes: locationNodes } = useGLTF('/media/location.glb');
     const locationMaterialRef = useRef(null);
-
-
-    //Traansformer le mesh en points
-    // let sampler 
-    // const vertices = []
-    // const tempPosition = new THREE.Vector3()
-    // let pointsGeometry = new THRE.BufferGeometry()
-    // function transformMesh(){
-    //     for(let i = 0; i<9000; i++){
-    //         sampler.sample(tempPosition);
-    //         vertices.push(tempPosition.x,tempPosition.y,tempPosition.z)
-    //     }
-    //     pointsGeometry.setAttribute('position',new THREE.Float32BufferAttribute(vertices,3))
-    //     const pointsMaterial = new THREE.PointsMaterial({
-    //         color:0x5c0b17,
-    //         size:0.02,
-    //         blending:THREE.AdditiveBlending,
-    //         transparent:true,
-    //         opacity:0.8,
-    //         dephtWrite:false,
-    //         sizeAttenuation:true
-    //     })
-
-    //     const points = new THREE.Points(pointsGeometry,pointsMaterial)
-    //     scene.add(points)
-
-    // }
-
 
     // Store the initial rotation and animation completion state
     const [initialRotation, setInitialRotation] = useState({ x: 0, y: 0 });
@@ -53,69 +28,80 @@ export default function Model({ mousePosition }) {
     useEffect(() => {
         if (island.current && islandMaterialRef.current) {
 
-
-            const geometry = nodes.reunion.geometry;
-            const positions = geometry.attributes.position.array;
-            const indices = geometry.index.array;
-
-            // Convert geometry to line segments
-            const linesGeometry = new BufferGeometry();
-            const vertices = [];
-
-            for (let i = 0; i < indices.length; i += 2) {
-                const start = indices[i] * 3;
-                const end = indices[i + 1] * 3;
-
-                vertices.push(
-                    positions[start], positions[start + 1], positions[start + 2],
-                    positions[end], positions[end + 1], positions[end + 2]
-                );
-            }
-
-            linesGeometry.setAttribute('position', new Float32BufferAttribute(vertices, 3));
-            const linesMaterial = new LineBasicMaterial({ color: 0x5c0b17 });
-
-            setLines(new LineSegments(linesGeometry, linesMaterial));
-
-
             const initialRotationX = 5 * (Math.PI / 180);
             const initialRotationY = -75 * (Math.PI / 180);
 
             setInitialRotation({ x: initialRotationX, y: initialRotationY });
             island.current.rotation.set(initialRotationX, initialRotationY, 0);
 
+            island.current.transparent = true;
             islandMaterialRef.current.color.set('#0f4c5c');
-            islandMaterialRef.current.transparent = true;
             islandMaterialRef.current.opacity = 0.8;
             islandMaterialRef.current.map = texture;
             islandMaterialRef.current.depthTest = false;
+            // islandMaterialRef.current.side = 1; // THREE.FrontSide is 2, to make the material front-only
 
             // gsap.to(island.current.position, {
-            //     z: -3,
-            //     y: -0.8,
-            //     x: 1.5,
-            //     delay: 15,
+            //     // scrollTrigger: island,
+            //     z: -1,
+            //     y: -0.2,
+            //     x: 0.5,
+            //     delay: 10,
             //     opacity: 1,
             //     duration: 1,
-            //     onUpdate: () => {
+            //     onComplete: () => {
             //         setAnimationComplete(true);
             //         islandMaterialRef.current.transparent = false;
             //         islandMaterialRef.current.opacity = 1;
             //     }
             // });
+            // ScrollTrigger.create({
+            //     trigger: island.current,
+            //     start: 'top center',
+            //     end: 'bottom center',
+            //     onEnter: () => {
+            //         gsap.to(island.current.position, {
+            //             z: -1,
+            //             y: -0.2,
+            //             x: 0.5,
+            //             opacity: 1,
+            //             duration: 1,
+            //             onComplete: () => {
+            //                 setAnimationComplete(true);
+            //                 islandMaterialRef.current.transparent = false;
+            //                 islandMaterialRef.current.opacity = 1;
+            //             }
+            //         });
+            //     },
+            //     onLeaveBack: () => {
+            //         gsap.to(island.current.position, {
+            //             z: 0,
+            //             y: 0,
+            //             x: 0,
+            //             opacity: 0.8,
+            //             duration: 1,
+            //             onComplete: () => {
+            //                 setAnimationComplete(false);
+            //                 islandMaterialRef.current.transparent = true;
+            //                 islandMaterialRef.current.opacity = 0.8;
+            //             }
+            //         });
+            //     }
+            // });
+
         }
 
         // Animation de la respiration pour les textes
         if (textRef1.current && textRef2.current) {
             gsap.from(textRef1.current.material, {
-                opacity: 0.5,
+                opacity: 0.1,
                 duration: 1.7,
                 repeat: -1,
                 yoyo: true,
                 ease: 'power1.inOut'
             });
             gsap.from(textRef2.current.material, {
-                opacity: 0.5,
+                opacity: 0.1,
                 duration: 1.7,
                 repeat: -1,
                 yoyo: true,
@@ -123,22 +109,24 @@ export default function Model({ mousePosition }) {
             });
         }
 
-        if (location.current && locationMaterialRef.current) {
-            locationMaterialRef.current.color.set('#0000');
-            locationMaterialRef.current.transparent = false;
-            locationMaterialRef.current.depthTest = false;
-            locationMaterialRef.current.opacity = 1
-        }
     }, []);
 
 
 
     useFrame(() => {
         if (island.current) {
-            const rotationFactor = 0.4;
+
+            const rotationFactor = 0.2;
+            // const translationFactor = 0.12; // Ajustez cette valeur pour contrôler l'amplitude de la translation
 
             let rotationX = initialRotation.x - mousePosition.y * rotationFactor;
             let rotationY = initialRotation.y + mousePosition.x * rotationFactor;
+
+            // let translationX = mousePosition.x * translationFactor;
+            // let translationY = -mousePosition.y * translationFactor;
+
+            // island.current.position.x = translationX;
+            // island.current.position.y = -translationY;
 
             if (animationComplete) {
                 rotationX = initialRotation.x;
@@ -155,42 +143,44 @@ export default function Model({ mousePosition }) {
 
         if (location.current) {
             location.current.rotation.y += 0.01;
-            location.current.oppacity = 1
+            location.current.oppacity = inherits
+            locationMaterialRef.current.color.set('#0000');
+            locationMaterialRef.current.transparent = false;
         }
 
 
     });
 
     return (
-        <group scale={viewport.width / 3}>
-            <group>
-                <mesh ref={island} geometry={nodes.reunion.geometry} scale={[0.015, 0.015, 0.015]}>
-                    <meshStandardMaterial ref={islandMaterialRef} />
+            <group scale={viewport.width / 3}>
+                <group>
+                    <mesh ref={island} geometry={nodes.reunion.geometry} scale={[0.015, 0.015, 0.015]} object={lines}  >
+                        {/* {lines && <primitive object={lines} />} */}
+                        <meshStandardMaterial ref={islandMaterialRef} />
 
-                    <Text
-                        ref={textRef1}
-                        position={[20, 2, 16]}
-                        fontSize={2}
-                        color="#0000"
-                    >
-                        Reunion Island
-                    </Text>
-                    <Text
-                        ref={textRef2}
-                        position={[28, 0, 8]}
-                        fontSize={1.5}
-                        color="#0000"
-                    >
-                        Le Tampon
-                        <mesh ref={location} geometry={locationNodes.location.geometry} scale={[0.1, 0.1, 0.1]} position={[0, -2, 3]} >
-                            <meshStandardMaterial ref={locationMaterialRef} />
-                        </mesh>
-                    </Text>
-                    <pointLight position={[30, -4, 8]} intensity={10} color={'red'} />
-                </mesh>
+                        <Text
+                            ref={textRef1}
+                            position={[20, 2, 16]}
+                            fontSize={2}
+                            color="#0000"
+                        >
+                            Reunion Island
+                        </Text>
+                        <Text
+                            ref={textRef2}
+                            position={[28, 0, 8]}
+                            fontSize={1.5}
+                            color="#0000"
+                        >
+                            Le Tampon
+                            <mesh ref={location} geometry={locationNodes.location.geometry} scale={[0.1, 0.1, 0.1]} position={[0, -2, 3]} >
+                                <meshStandardMaterial ref={locationMaterialRef} />
+                            </mesh>
+                        </Text>
+                        <pointLight position={[30, -4, 8]} intensity={10} color={'red'} />
+                    </mesh>
+                </group>
             </group>
 
-
-        </group>
     );
 }
--- app/utils/Scene.jsx
+++ app/utils/Scene.jsx
@@ -5,25 +5,59 @@ import Model from './Model';
 import { OrbitControls, Environment } from '@react-three/drei';
 import { gsap } from 'gsap';
 import Image from 'next/image';
+import { ScrollTrigger } from 'gsap/all';
 import logo from '../../assets/LogoExoticDigitalStudioWhite.png';
 
 export default function Scene() {
   const divRef = useRef(null);
   const divRef2 = useRef(null);
   const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
+  const island = useRef(null);
 
   useEffect(() => {
     // Animation de la couleur de fond du noir à transparent
-    gsap.to(divRef.current, {
-      backgroundColor: 'rgba(0, 0, 0, 0)',
-      duration: 2,
-      delay: 1500,
-      zIndex: 0,
-      ease: "power4.inOut",
-      onStart: () => {
+    ScrollTrigger.create({
+      trigger: divRef.current,
+      start: 'top top',
+      end: '+=500',
+      scrub: 1,
+      onEnter: () => {
+        gsap.to(divRef.current, {
+          backgroundColor: 'rgba(0, 0, 0, 0)',
+          duration: 2,
+          zIndex: 0,
+          ease: 'power4.inOut',
+          onStart: () => {
+            gsap.to(divRef2.current, {
+              opacity: 0,
+            });
+          },
+        });
+        // Animation de l'île lors du défilement
+        gsap.to(island.current.position, {
+          x: 0.5,
+          y: -0.2,
+          z: -1,
+          duration: 2,
+          ease: 'power4.inOut',
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to(divRef.current, {
+          backgroundColor: 'rgba(0, 0, 0, 1)',
+          duration: 2,
+          zIndex: 206,
+          ease: 'power4.inOut',
+        });
         gsap.to(divRef2.current, {
-          // display: "none",
-          opacity: 0,
+          opacity: 1,
+        });
+        gsap.to(island.current.position, {
+          x: 0,
+          y: 0,
+          z: 0,
+          duration: 2,
+          ease: 'power4.inOut',
         });
       },
     });
@@ -43,25 +77,25 @@ export default function Scene() {
   }, []);
 
   // Numeric counter 
-  // let counterElement;
-  // let currentValue = 0;
-  // const totalDuration = 15000; // Durée totale en ms (10 secondes)
-  // const updatesCount = 100; // Nombre total de mises à jour
-  // const delay = totalDuration / updatesCount; // Délai entre chaque mise à jour
+  let counterElement;
+  let currentValue = 0;
+  const totalDuration = 15000; // Durée totale en ms (10 secondes)
+  const updatesCount = 100; // Nombre total de mises à jour
+  const delay = totalDuration / updatesCount; // Délai entre chaque mise à jour
 
-  // function startLoader() {
-  //   counterElement = document.getElementById('couter-number');
-  //   updateCounter();
-  // }
+  function startLoader() {
+    counterElement = document.getElementById('couter-number');
+    updateCounter();
+  }
 
-  // function updateCounter() {
-  //   if (currentValue >= 100) {
-  //     return;
-  //   }
-  //   currentValue++;
-  //   counterElement.textContent = currentValue;
-  //   setTimeout(updateCounter, delay); // Utilisation du délai calculé
-  // }
+  function updateCounter() {
+    if (currentValue >= 100) {
+      return;
+    }
+    currentValue++;
+    counterElement.textContent = currentValue;
+    setTimeout(updateCounter, delay); // Utilisation du délai calculé
+  }
 
   function CameraAnimation() {
     const { camera } = useThree();
@@ -75,51 +109,51 @@ export default function Scene() {
       //   ease: 'power4.inOut',
       // });
 
-      gsap.fromTo("#counter", {
+      gsap.from("#counter", {
         width: `${0}%`,
-      }, {
-        opacity: 0,
-        width: `${100}%`,
-        duration: 5,
+        duration: 10,
         stagger: 0.1,
+        opacity: 0,
+
       });
 
-    }, [camera]);
+    }, []);
 
     return null;
   }
 
-  // useEffect(() => {
-  //   gsap.to("#counter-number",{
-  //     ease: "power4.Out",
-  //     opacity:0
-  //   });
+  useEffect(() => {
+    gsap.to("#counter-number",{
+      ease: "power4.Out",
+      opacity:0
+    });
 
-  //   startLoader();
-  // }, []);
+    startLoader();
+  }, []);
 
   return (
     <div ref={divRef} className='h-screen fixed flex-col fixed bg-black items-center justify-center w-full flex z-[206]'>
       <div ref={divRef2} className='fixed top-0 left-0 h-screen w-screen'>
         <Image src={logo} alt="logo de la compagnie" width={320} height={50} />
         {/* <h1 className='ml-20'> Loading . . .</h1> */}
-        {/* <h1 id="couter-number" className='ml-20 text-6xl'>0</h1> */}
-        {/* <div>
+        <h1 id="couter-number" className='ml-20 text-6xl'>0</h1>
+        <div>
           <div className=' ml-20 h-10 bg-white w-[400px]'>
             <div id='counter' className='w-full h-full bg-gray-900 border-solid border-x-2 border-y-2 border-white'>
             </div>
           </div>
           <div>
           </div>
-        </div> */}
+        </div>
       </div>
+
       <Canvas camera={{ position: [0,0,6]}}>
         <Suspense fallback={null}>
-          <Model mousePosition={mousePosition}/>
+          <Model mousePosition={mousePosition}  island={island}/>
         </Suspense>
         <ambientLight castShadow position={[0, 0, 1]} intensity={9} color={'white'} />
         <Environment preset="forest" />
-        <OrbitControls />
+        {/* <OrbitControls /> */}
         <CameraAnimation />
       </Canvas>
     </div>
