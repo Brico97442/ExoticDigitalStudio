b0abd15e5e57384442e587e8b37077aba2955f4a
Damien <amanvilledamien@hotmail.fr>
2024-08-20 21:20:11 +0400

    fix reveal card

---
+++ app/components/GridAnimation.jsx
@@ -0,0 +1,83 @@
+'use client';
+
+import React, { useEffect, useRef } from 'react'
+import Image from 'next/image'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Img from '../../public/media/galerie-item-2.jpg'
+import Img2 from '../../public/media/galerie-item-3.jpg'
+import Img3 from '../../public/media/galerie-item-4.jpg'
+import Img4 from '../../public/media/galerie-item-5.jpg'
+
+gsap.registerPlugin(ScrollTrigger)
+
+export default function GridAnimation() {
+    const containerRef = useRef(null)
+    const imageRefs = useRef([])
+
+    useEffect(() => {
+        const images = imageRefs.current.filter(Boolean)
+
+        images.forEach((img, index) => {
+            const isReverse = index === 2 || index === 4 || index === 0
+
+            // Initial state
+            gsap.set(img, {
+                clipPath: isReverse 
+                    ? 'polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)' // Start from the right
+                    : 'polygon(0% 0%, 0% 0%, 0% 100%, 0% 100%)', // Start from the left
+            })
+
+            // Animation
+            gsap.to(img, {
+                clipPath: 'polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)', // Full reveal
+                ease: 'power4.inOut',
+                duration: 5,
+                scrollTrigger: {
+                    trigger: img,
+                    start: '40% 80%', // Ajuste le déclencheur pour une meilleure visibilité
+                    end: 'bottom 50%',
+                    scrub: 1,
+                    markers: true
+                }
+            })
+        })
+
+        return () => {
+            ScrollTrigger.getAll().forEach(trigger => trigger.kill())
+        }
+    }, [])
+
+    return (
+        <div id='pricing' className='container h-full w-full flex flex-col items-center justify-center z-[1] m-auto'>
+            <div ref={containerRef} className='w-[85vw] m-auto mt-60'>
+                <div className='w-full'>
+                    <div className='card-img-container'>
+                        <div className='card-img justify-end overflow-hidden'>
+                            <Image ref={el => imageRefs.current[0] = el} src={Img} alt='Grid Element' />
+                        </div>
+                        <p>Title</p>
+                    </div>
+                    <div className='card-img-container justify-end'>
+                        <p>Title</p>
+                        <div className='card-img justify-start h-1/2 overflow-hidden'>
+                            <Image ref={el => imageRefs.current[1] = el} src={Img2} alt='Grid Element'/>
+                        </div>
+                    </div>
+                    <div className='card-img-container'>
+                        <div className='card-img justify-end overflow-hidden'>
+                            <Image ref={el => imageRefs.current[2] = el} src={Img3} alt='Grid Element'/>
+                        </div>
+                        <p>Title</p>
+                    </div>
+                    <div className='card-img-container justify-end'>
+                        <p>Title</p>
+                        <div className='card-img overflow-hidden'>
+                            <Image ref={el => imageRefs.current[3] = el} src={Img4} alt='Grid Element' />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -6,12 +6,10 @@ import Lenis from 'lenis';
 import { useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import Lines from './components/Lines'
-import Slideshow from './components/Slider'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Carousel from './components/Carousel'
+
 import ContactForm from './components/ContactForm'
 import TextScroll from "./components/TextScroll";
+import GridAnimation from "./components/GridAnimation";
 
 const Scene = dynamic(() => import('./utils/Scene'), {
   ssr: false
@@ -47,12 +45,12 @@ export default function Home() {
           <h1 id='hero-title' className=' pointer-events-none text-[12.5em] leading-none	tracking-tighter  z-[3] w-full text-black '>Ui<span className=" bg-clip-text text-transparent bg-gradient-to-r from-[#A4161A] to-[#666666]"><span className="text-[0.6em]">&</span>Ux</span></h1>
           <h2 className='z-[3] text-[2.5em] mt-6 leading-none w-2/3'> Offrez à vos visiteurs une aventure web captivante et inoubliable parce que chaque clic mérite une touche de magie.</h2>
         </div>
-        <Button position='mb-[200px] right-20 bottom-0 absolute'/>
+        <Button position='mb-[200px] right-20 bottom-0 absolute' />
       </div>
       <div id="about" className="h-screen flex flex-col justify-center items-start w-full max-w-[85vw] m-auto relative z-[3] sticky">
         <h1 id='about-title' className='text-[9.375em] z-[3] bg-clip-text text-align-left tracking-tight my-20 bg-gradient-to-r from-black to-neutral-950/80'>Qui somme nous ?</h1>
         <div className='w-full flex justify-end'>
-          <Lines  strokeColor="stroke-black"/>
+          <Lines strokeColor="stroke-black" />
         </div>
         <div className='h-full w-full flex flex-col justify-between sticky z-[3]'>
           <div className="w-full flex justify-end">
@@ -66,29 +64,20 @@ export default function Home() {
         </div>
       </div>
       <Hero target={targetRef} />
-      <div className="z-[4] h-screen w-full sticky">
-        <div id="3d-carousel" className="w-full h-screen sticky relative">
-          <div className="w-full absolute top-0 left-0 z-[5]">
-            <div className="max-w-[85vw] m-auto">
-              <h1 className="text-[12.5em] mt-20 ">Nos Prestations</h1>
-        <Button position="absolute right-[6vw] top-[20vh]"/>
-            </div>
+      <div className="z-[2] min-h-screen h-full w-full sticky">
+          <div className="z-[2] w-[85vw] m-auto">
+              <h1 className="text-[12.5em] ">Nos Prestations</h1>
+              <Button position="absolute right-[6vw] top-[20vh]" />
+            <GridAnimation />
           </div>
-          <div className="h-screen w-full flex z-[1]">
-            <Canvas>
-              <Suspense fallback={null}>
-                <Carousel />
-              </Suspense>
-            </Canvas>
-          </div>
-        </div>
       </div>
-      <div id="contact" className="z-[4] max-w-[85vw] m-auto w-full min-h-screen relative">
+
+      <div id="contact" className="z-[2] max-w-[85vw] m-auto w-full min-h-screen relative">
         <h1 className="text-[12.5em] mt-10 mb-10 leading-none">Démarrons</h1>
         <h1 className="text-[12.5em] mb-10 leading-none">un projet</h1>
         <h2 className="absolute right-0 top-[35vh] text-[2em] w-1/4 text-right">"Les grandes histoires commençent souvent par un Hey!"</h2>
-        <Lines strokeColor="stroke-black"/>
-        <ContactForm/>
+        <Lines strokeColor="stroke-black" />
+        <ContactForm />
       </div>
       {/* )} */}
     </main>
