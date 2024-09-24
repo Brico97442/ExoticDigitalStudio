2038fa7b76db0b47a7a66077791e0bb7c0c8e389
Damien <amanvilledamien@hotmail.fr>
2024-09-24 17:12:20 +0400

    edit contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import ContactForm from "../components/ContactForm";
 
 export default function Contact() {
     return (
-        <section className="flex justify-center items-center w-full h-screen ">
+        <section className="flex justify-center items-center w-full h-screen px-40">
             <div className=" w-full flex flex-col gap-10 z-[3]">
                 <div className="w-full gap-6 flex flex-col">
                     <h1 className="text-[8vh] leading-none">Dites m'en un peu plus</h1>
